3f348894a902f579b0aa79f1db8d51b6baf2743c
daya <dayajuangmahaputra@gmail.com>
2022-11-09 16:30:05 +0900

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,27 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean ans = isPalindrome(-1);
+        System.out.println(ans);
+    }
+
+    public static boolean isPalindrome(int number){
+        // Check if number digit is greater than 1
+        if(number / 10 == 0)
+            return false;
+
+        // Make a reverse of number
+        int temp = Math.abs(number);
+        int reverse = 0;
+
+        while (temp > 0){
+            reverse *= 10;
+            reverse += (temp % 10);
+            temp /= 10;
+        }
+
+        // Check if the number and reverse is the same
+        return reverse == Math.abs(number);
     }
 }
